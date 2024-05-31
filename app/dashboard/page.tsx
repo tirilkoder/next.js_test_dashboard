88d2d034001b19a8e91ecf2c28c53121c39c9666
Tiril Myksvoll <tiril.koder@gmail.com>
2024-05-31 10:20:36 +0200

    design fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,19 +6,34 @@ export default function Page() {
   return (
     <>
       <div className="flex justify-center">
-        <h1 className="mb-5 text-xl font-bold text-gray-800 md:text-3xl md:leading-normal">
+        <h1 className="mb-5 font-sans text-xl font-bold text-gray-800 md:text-3xl md:leading-normal">
           Velkommen
         </h1>
       </div>
 
-      <div className="relative m-auto max-w-5xl">
-        <div className="">
+      <div className="relative m-auto max-w-5xl justify-center">
+        <div className="hidden">
           <div className="absolute p-0 px-3 text-sm"> 1/3</div>
 
           <Image
             src="/graph.jpg"
-            width={460}
-            height={520}
+            width={1000}
+            height={1000}
+            className="m-2 rounded"
+            alt="Produkter"
+          />
+          <div className="absolute w-full px-3 pb-2 text-center text-sm">
+            Beskrivelse
+          </div>
+        </div>
+
+        <div className="hidden">
+          <div className="absolute p-0 px-3 text-sm"> 2/3</div>
+
+          <Image
+            src="/graph.jpg"
+            width={1000}
+            height={1000}
             className="m-2 rounded"
             alt="Produkter"
           />
@@ -28,27 +43,12 @@ export default function Page() {
         </div>
 
         <div className="">
-          <div className="absolute p-0 px-3 text-sm"> 1/3</div>
+          <div className="absolute p-0 px-3 text-sm"> 3/3</div>
 
           <Image
             src="/graph.jpg"
-            width={460}
-            height={520}
-            className="m-2 rounded"
-            alt="Produkter"
-          />
-          <div className="absolute w-full px-3 pb-2 text-center text-sm">
-            Beskrivelse
-          </div>
-        </div>
-
-        <div className="">
-          <div className="absolute p-0 px-3 text-sm"> 1/3</div>
-
-          <Image
-            src="/graph.jpg"
-            width={460}
-            height={520}
+            width={900}
+            height={900}
             className="m-2 rounded"
             alt="Produkter"
           />
@@ -62,11 +62,17 @@ export default function Page() {
           <a className="next" onClick={plusSlides(1)}>&#10095;</a>
         </div>
         <br>
-      
+         <!-- Dots at the bottom --> 
         <div className="text-center">
   <span className="dot" onClick={currentSlide(1)}></span>
   <span className="dot" onClick={currentSlide(2)}></span>
   <span className="dot" onClick={currentSlide(3)}></span>*/}
+      </div>
+
+      <div className="flex justify-center">
+        <h2 className="mb-5 text-xl font-bold text-gray-800 md:text-3xl md:leading-normal">
+          Populære produkter
+        </h2>
       </div>
     </>
   );
